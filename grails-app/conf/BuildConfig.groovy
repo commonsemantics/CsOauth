cdf8f0975788b233202b403401d976a23373f1b0
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-06-06 15:55:45 -0400

    Adjusted dependencies (maven repositories and CsSystems project).

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.plugin.location.'cs-systems' = '../../commonsemantics/CsSystems'
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -17,9 +19,12 @@ grails.project.dependency.resolution = {
         //mavenLocal()
         //mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
+       
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+		mavenRepo "http://repository.codehaus.org"
+		mavenRepo "http://repo.spring.io/milestone/"
+		mavenRepo "http://maven.springframework.org/release/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
