9d14e015afdda56b4c2240b17dde562a2a2b5fbc
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-05-29 14:20:06 +0100

    Enabled 'password' grant type

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,46 +1,55 @@
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.commonsemantics.grails.security.oauth.OAuthAuthorizationCodeTokenGranter;
 import org.commonsemantics.grails.security.oauth.OAuthClientCredentialsAuthenticationProvider;
+import org.commonsemantics.grails.security.oauth.ResourceOwnerPasswordTokenGranter;
 import org.commonsemantics.grails.security.oauth.OAuthClientDetailsService;
 import org.commonsemantics.grails.security.oauth.OAuthTokenStore;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenGranter;
 import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.refresh.RefreshTokenGranter;
 
 beans = {
-	
+
 	// OAuth
 	customOAuthAuthenticationProvider(OAuthClientCredentialsAuthenticationProvider)
-	
+
 	clientDetailsService(OAuthClientDetailsService) {
 		grailsApplication = ref("grailsApplication")
 	}
 
 	tokenStore(OAuthTokenStore)
-	
+
 	tokenServices(DefaultTokenServices) {
 		tokenStore = ref("tokenStore")
 		supportRefreshToken = "true"
 		clientDetailsService = ref("clientDetailsService")
 		accessTokenValiditySeconds = 86400
 	}
-	
-	authorizationCodeServices(InMemoryAuthorizationCodeServices)
-	
-	oauth2TokenGranter1(OAuthAuthorizationCodeTokenGranter,
-            tokenServices = ref("tokenServices"),
-            authorizationCodeServices = ref("authorizationCodeServices"),
-            clientDetailsService = ref("clientDetailsService"))
 
-    oauth2TokenGranter2(RefreshTokenGranter,
-            tokenServices = ref("tokenServices"),
-            clientDetailsService = ref("clientDetailsService"))
-	
+	authorizationCodeServices(InMemoryAuthorizationCodeServices)
+
+	oauth2TokenGranter1(OAuthAuthorizationCodeTokenGranter,
+			tokenServices = ref("tokenServices"),
+			authorizationCodeServices = ref("authorizationCodeServices"),
+			clientDetailsService = ref("clientDetailsService"))
+
+	oauth2TokenGranter2(RefreshTokenGranter,
+			tokenServices = ref("tokenServices"),
+			clientDetailsService = ref("clientDetailsService"))
+
 	oauth2TokenGranter3(ClientCredentialsTokenGranter,
 			tokenServices = ref("tokenServices"),
 			clientDetailsService = ref("clientDetailsService"))
 
-    oauth2TokenGranter(CompositeTokenGranter, [ref("oauth2TokenGranter1"), ref("oauth2TokenGranter2"), ref("oauth2TokenGranter3")])
-	
+	oauth2TokenGranter4(ResourceOwnerPasswordTokenGranter,
+			authenticationManager = ref("authenticationManager"),
+			tokenServices = ref("tokenServices"),
+			clientDetailsService = ref("clientDetailsService"))
+
+	oauth2TokenGranter(CompositeTokenGranter, [ref("oauth2TokenGranter1"), ref("oauth2TokenGranter2"), ref("oauth2TokenGranter3"), ref("oauth2TokenGranter4")])
+
 }
